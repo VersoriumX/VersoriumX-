27d0e59fb9e27182f1412f484711de4769d01791
HelLuv <works.person@gmail.com>
2022-02-21 15:42:57 +0300

    finished with russian localization

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -2,6 +2,9 @@ import {Footer, Navbar, Services, Transactions, Welcome} from "./components";
 import './i18n';
 
 const App = () => {
+
+  //TODO: add some animation via framer motion
+
   return (
     <div className="min-h-screen">
       <div className="gradient-bg-welcome">
